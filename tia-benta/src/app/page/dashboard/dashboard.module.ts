3987ba63e9e5a63484fab1f37b84e62d09741c6c
depaualuis <henriquesilvadepaula@gmail.com>
2021-04-14 07:59:30 -0300

    Adicionando a entidade, CRUD e service para User

--- tia-benta/src/app/page/dashboard/dashboard.module.ts
+++ tia-benta/src/app/page/dashboard/dashboard.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 import { DashboardRoutingModule } from './dashboard-routing.module';
 import { SharedModule } from '../../shared/shared.module';
 import { IndexDashComponent } from './index-dash/index-dash.component';
+import { UserDetailComponent } from './user/user-detail/user-detail.component';
+import { UserFormComponent } from './user/user-form/user-form.component';
+import { UserListComponent } from './user/user-list/user-list.component';
+import { UserViewComponent } from './user/user-view/user-view.component';
 
 
 @NgModule({
-  declarations: [IndexDashComponent],
+  declarations: [IndexDashComponent, UserDetailComponent, UserFormComponent, UserListComponent, UserViewComponent],
   imports: [
     SharedModule,
     DashboardRoutingModule
