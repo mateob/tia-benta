411c31615186e83ecec487787e607c2bba2e9e62
Guilherme Pinheiro <guilhermemarkevis@hotmail.com>
2021-04-22 09:08:59 -0300

    Adicionando Alias e rotas para role

--- tia-benta/src/app/page/dashboard/dashboard-routing.module.ts
+++ tia-benta/src/app/page/dashboard/dashboard-routing.module.ts
@@ -5,7 +5,8 @@ const routes: Routes = [
   { path: 'customer', loadChildren: () => import('@pages/dashboard/customer.module').then((m) => m.CustomerModule) },
   { path: 'recipe', loadChildren: () => import('@pages/dashboard/recipe.module').then((m) => m.RecipeModule) },
   { path: 'item', loadChildren: () => import('@pages/dashboard/item.module').then((m) => m.ItemModule) },
-  { path: 'user-group', loadChildren: () => import('@pages/dashboard/user-group.module').then((m) => m.UserGroupModule) }
+  { path: 'user-group', loadChildren: () => import('@pages/dashboard/user-group.module').then((m) => m.UserGroupModule) },
+  { path: 'role', loadChildren: () => import('@pages/dashboard/role.module').then((m) => m.RoleModule) }
 ];
 
 @NgModule({
--- tia-benta/src/app/page/dashboard/dashboard.module.ts
+++ tia-benta/src/app/page/dashboard/dashboard.module.ts
@@ -3,13 +3,9 @@ import { NgModule } from '@angular/core';
 import { DashboardRoutingModule } from './dashboard-routing.module';
 import { SharedModule } from '../../shared/shared.module';
 import { IndexDashComponent } from './index-dash/index-dash.component';
-import { RoleDetailComponent } from './role/role-detail/role-detail.component';
-import { RoleFormComponent } from './role/role-form/role-form.component';
-import { RoleListComponent } from './role/role-list/role-list.component';
-import { RoleViewComponent } from './role/role-view/role-view.component';
 
 @NgModule({
-  declarations: [IndexDashComponent, RoleDetailComponent, RoleFormComponent, RoleListComponent, RoleViewComponent],
+  declarations: [IndexDashComponent],
   imports: [
     SharedModule,
     DashboardRoutingModule
