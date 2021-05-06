66d7fbb29b877583fd71160aa7df4d02813997e4
Guilherme Pinheiro <guilhermemarkevis@hotmail.com>
2021-05-06 08:24:21 -0300

    Adicionando Base Form/List/View as telas da Entidade User-Group

--- tia-benta/src/app/page/dashboard/user-group/user-group-detail/user-group-detail.component.ts
+++ tia-benta/src/app/page/dashboard/user-group/user-group-detail/user-group-detail.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Injector } from '@angular/core';
+
+import { UserGroupModel } from '@model/user-group.model';
+import { BaseFormComponent } from '../../../../shared/component/base-component/base-form.component';
+import { UserGroupService } from '../shared/user-group.service';
 
 @Component({
-  selector: 'app-user-group-detail',
+  selector: 'app-customer-detail',
   templateUrl: './user-group-detail.component.html',
   styleUrls: ['./user-group-detail.component.scss']
 })
-export class UserGroupDetailComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+export class UserGroupDetailComponent extends BaseFormComponent<UserGroupModel> {
+  protected validateBeforeAction(): boolean {
+    return true;
   }
 
+  protected onModalSave(): boolean {
+    return true;
+  }
+
+  protected onModalCancel(): boolean {
+    return true;
+  }
+
+  constructor(protected injector: Injector, protected userGroupService: UserGroupService) {
+    super(injector, new UserGroupModel(), userGroupService);
+  }
 }
