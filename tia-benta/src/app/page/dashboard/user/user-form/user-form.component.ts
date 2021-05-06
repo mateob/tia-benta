e4b3f8dc69ec48e0fa6917b9e660f3c5cfe30adb
Luis Henrique <henriquesilvadepaula@gmail.com>
2021-05-06 08:24:49 -0300

    Criando a herança e CRUD, corrigindo tambem o user.service.ts e user-group.service.ts

--- tia-benta/src/app/page/dashboard/user/user-form/user-form.component.ts
+++ tia-benta/src/app/page/dashboard/user/user-form/user-form.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Injector } from '@angular/core';
+import { UserModel } from '@model/user.model';
+import { UserService } from '../shared/user.service';
+import { BaseFormComponent } from '../../../../shared/component/base-component/base-form.component';
 
 @Component({
   selector: 'app-user-form',
   templateUrl: './user-form.component.html',
   styleUrls: ['./user-form.component.scss']
 })
-export class UserFormComponent implements OnInit {
+export class UserFormComponent extends BaseFormComponent<UserModel> {
 
-  constructor() { }
-
-  ngOnInit(): void {
+  protected validateBeforeAction(): boolean {
+    return true;
   }
 
+  protected onModalSave(): boolean {
+    return true;
+  }
+
+  protected onModalCancel(): boolean {
+    return true;
+  }
+
+  constructor(protected injector: Injector, protected userService: UserService) {
+    super(injector, new UserModel(), userService);
+  }
 }
