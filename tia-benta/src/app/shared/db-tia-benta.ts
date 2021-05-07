6a5384447bf0b5dfd84574ceb7667ed9db914998
Guilherme Pinheiro <guilhermemarkevis@hotmail.com>
2021-05-07 08:22:28 -0300

    Adicionando registros para UserGroup em DBTiaBenta.ts

--- tia-benta/src/app/shared/db-tia-benta.ts
+++ tia-benta/src/app/shared/db-tia-benta.ts
@@ -1,6 +1,7 @@
 import { InMemoryDbService, RequestInfo } from 'angular-in-memory-web-api';
 import { Observable } from 'rxjs';
 import { CustomerModel } from './model/customer.model';
+import { UserGroupModel } from './model/user-group.model';
 
 export class DBTiaBenta implements InMemoryDbService {
   createDb(reqInfo?: RequestInfo): {} | Observable<{}> | Promise<{}> {
@@ -12,10 +13,23 @@ export class DBTiaBenta implements InMemoryDbService {
       { id: '6123', name: 'Teste 5', document: '6123' } as CustomerModel,
     ]
 
+    const userGroup: UserGroupModel[] = [
+      { id: '1234', name: 'Gerente'} as UserGroupModel,
+      { id: '9123', name: 'Caixa'} as UserGroupModel,
+      { id: '8123', name: 'Entregador'} as UserGroupModel,
+      { id: '7123', name: 'Garçom'} as UserGroupModel,
+      { id: '6123', name: 'Cozinheiro'} as UserGroupModel,
+      { id: '5123', name: 'Aux. Cozinha'} as UserGroupModel,
+      { id: '4123', name: 'Cliente'} as UserGroupModel,
+      { id: '3123', name: 'Fornecedor'} as UserGroupModel,
+      { id: '2123', name: 'Nutricionista'} as UserGroupModel,
+      { id: '0123', name: 'Aux. Limpeza'} as UserGroupModel
+    ]
+
     const token: any[] = [
       { id: '', userName: '', password: '', name: '', roles: [], userGroupId: '', token: '' }
     ];
-    return { customer };
+    return { customer, userGroup };
   }
 
 }
