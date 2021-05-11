a39c150e818dc572bbd615455b880939e41b3e42
Guilherme Pinheiro <guilhermemarkevis@hotmail.com>
2021-05-11 18:25:47 -0300

    Corrigindo selector user-group-view

--- tia-benta/src/app/page/dashboard/user-group/user-group-view/user-group-view.component.ts
+++ tia-benta/src/app/page/dashboard/user-group/user-group-view/user-group-view.component.ts
@@ -5,7 +5,7 @@ import { UserGroupService } from '../shared/user-group.service';
 import { BaseViewComponent } from '../../../../shared/component/base-component/base-view.component';
 
 @Component({
-  selector: 'app-customer-view',
+  selector: 'app-user-group-view',
   templateUrl: './user-group-view.component.html',
   styleUrls: ['./user-group-view.component.scss']
 })
